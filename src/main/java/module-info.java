c17e6d93eb3d9f8910645e16f8c855a79281a507
Jannis Lauerwald <jannis1lauerwald@gmail.com>
2024-01-18 21:34:05 +0100

    Deleted Example Setting

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,11 @@
-module org.example.javafxlearn {
+module UI {
     requires javafx.controls;
     requires javafx.fxml;
 
 
-    opens org.example.javafxlearn to javafx.fxml;
-    exports org.example.javafxlearn;
-
     opens UI to javafx.fxml, javafx.graphics;
-
     exports UI;
+
 
     // And other module-related declarations...
 
