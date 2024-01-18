7fd67839d912efad8fe5852ae4e0c93ba7157c2c
Jannis Lauerwald <jannis1lauerwald@gmail.com>
2024-01-18 21:54:55 +0100

    add Label Object and changing Font-Size and Font-Color

--- src/main/java/UI/FirstUITest.java
+++ src/main/java/UI/FirstUITest.java
@@ -3,8 +3,11 @@ package UI;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class FirstUITest extends Application {
@@ -16,9 +19,15 @@ public class FirstUITest extends Application {
     public void start(Stage stage) throws Exception {
         VBox vBox = new VBox();
         Scene scene = new Scene(vBox, 700, 600);
+        Label label = new Label("Test, Test");
+        label.setTextFill(Color.WHITE);
+        VBox.setMargin(label, new javafx.geometry.Insets(0, 100, 0, 300)); // Ändere die Position um 20 Pixel von oben und 50 Pixel von links
+        label.setFont(new Font(30));
+        vBox.setStyle("-fx-background-color: blue;");
         Button button = new Button("Test");
         ProgressBar progressBar = new ProgressBar();
         progressBar.setProgress(0.25);
+        vBox.getChildren().add(label);
         vBox.getChildren().add(button);
         vBox.getChildren().add(progressBar);
         stage.setScene(scene);
